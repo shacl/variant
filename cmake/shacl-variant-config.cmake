bd7c4f56104edb5ce5cee158c0ae7a97ec6d26f7
Austin McCartney <amccartney@lanl.gov>
2019-05-08 08:45:19 -0600

    Fix from Colin + Ctest

--- cmake/shacl-variant-config.cmake
+++ cmake/shacl-variant-config.cmake
@@ -9,7 +9,7 @@ include("${CMAKE_CURRENT_LIST_DIR}/shacl-variant-targets.cmake")
 
 find_package(mpark_variant ${shacl-variant_FIND_QUIETLY_ARG})
 if(mpark_variant_FOUND)
-  set_target_property(shacl::variant INTERFACE_CXX_STANDARD 14)
+  set_target_properties(shacl::variant PROPERTIES INTERFACE_CXX_STANDARD 14)
   target_link_libraries(shacl::variant-14 INTERFACE mpark_variant)
   target_link_libraries(shacl::variant INTERFACE
     $<$<VERSION_LESS:$<TARGET_PROPERTY:CXX_STANDARD>,17>:shacl::variant-14>)
@@ -18,7 +18,7 @@ else()
   if(NOT "${shacl-optional_FIND_QUIETLY_ARG}")
     message(WARNING "shacl::variant C++-14 support requires mpark_variant")
   endif()
-  set_target_property(shacl::variant INTERFACE_CXX_STANDARD 17)
+  set_target_properties(shacl::variant PROPERTIES INTERFACE_CXX_STANDARD 17)
 endif()
 
 find_package(Boost 1.69 ${shacl-variant_FIND_QUIETLY_ARG})
